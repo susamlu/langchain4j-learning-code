f97c0ea02635aba4f9de01f30302f36a9bc040e1
Sam Lu <luxiaocong524@163.com>
2025-12-15 13:06:42 +0800

    first commit

---
+++ src/main/java/io/github/susamlu/langchain4j/HelloLangChain4jDemoKey.java
@@ -0,0 +1,18 @@
+package io.github.susamlu.langchain4j;
+
+import dev.langchain4j.model.openai.OpenAiChatModel;
+
+public class HelloLangChain4jDemoKey {
+
+    public static void main(String[] args) {
+        OpenAiChatModel model = OpenAiChatModel.builder()
+                .baseUrl("http://langchain4j.dev/demo/openai/v1")
+                .apiKey("demo")
+                .modelName("gpt-4o-mini")
+                .build();
+
+        String answer = model.chat("Say 'Hello World'");
+        System.out.println(answer);
+    }
+
+}
