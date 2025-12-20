cea598e5d1cc5fd1b536d7cf7747a4fd94052291
Sam Lu <luxiaocong524@163.com>
2025-12-20 23:44:00 +0800

    feature: 添加多轮对话、多模态、工具调用、流式输出等示例代码

---
+++ src/main/java/io/github/susamlu/langchain4j/HelloChatModel.java
@@ -0,0 +1,28 @@
+package io.github.susamlu.langchain4j;
+
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+
+public class HelloChatModel {
+
+    public static void main(String[] args) {
+        String apiKey = System.getenv("DEEPSEEK_API_KEY");
+
+        OpenAiChatModel model = OpenAiChatModel.builder()
+                .baseUrl("https://api.deepseek.com/v1")
+                .apiKey(apiKey)
+                .modelName("deepseek-chat")
+                .build();
+
+        UserMessage firstUserMessage = UserMessage.from("我叫 Sam");
+        AiMessage firstAiMessage = model.chat(firstUserMessage).aiMessage();
+        System.out.println(firstAiMessage.text());
+
+        UserMessage secondUserMessage = UserMessage.from("我叫什么名字？");
+        AiMessage secondAiMessage = model.chat(firstUserMessage, firstAiMessage, secondUserMessage)
+                .aiMessage();
+        System.out.println(secondAiMessage.text());
+    }
+
+}
